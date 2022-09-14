038402dfed455f18d3acb83a29ad4504b8216df1
manishyadav910 <manishyadav910@gmail.com>
2022-09-14 09:11:12 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,6 +2,7 @@ class Demo{
   
   public static void main(String args[])
   {
+    System.out.println("Hello world\n");
     System.out.println("This is first step towards automating the build ");
   }
 }
